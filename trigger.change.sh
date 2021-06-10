4e9989ae73abe0e246391b1f0feb7f4078783666
Your Name <you@example.com>
2021-06-10 13:18:52 +0300

    uncomment trigger

--- trigger.change.sh
+++ trigger.change.sh
@@ -1,4 +1,4 @@
-#while true; do curl "http://localhost:8080/";echo -n "."; sleep 5; done
+while true; do curl "http://localhost:8080/";echo -n "."; sleep 5; done
 
 # echo ---
 # docker inspect --format "{{json .State.Health }}" timerservice | jq
